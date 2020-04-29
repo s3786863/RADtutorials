fcab8b5509051cacde76ac98f45e341c7d4bb9ef
Darius Baluyut <s3786863@student.rmit.edu.au>
2020-04-29 17:10:33 +1000

    fixed some test errors

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,7 +30,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert is_logged_in?
-    assert_redirect_to @user
+    assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
